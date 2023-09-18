82f57b7fd6f12d89a3958821001fe5cbda678307
janhavi04 <jumblejanhavi@gmail.com>
2023-09-18 18:32:45 +0530

    edit

--- swap.cpp
+++ swap.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 using namespace std;
+// using third variable
+/*
 void swap(int *x, int *y){
 
     int temp ;
@@ -18,4 +20,17 @@ int main(){
     swap(&a,&b);
     cout<<"Value of a: "<<a<<endl;
     cout<<"Value of b:"<<b;
+}
+*/
+
+//without using third variable
+
+int main(){
+    int x , y;
+    x = 4;
+    y = 5;
+    x = x+y; // 9
+    y = x-y; // 4
+    x = x-y; // 5
+    cout<<x<<"    " <<y<<endl;
 }
